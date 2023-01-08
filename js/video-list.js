ba884f07bb335457373e7844811cf03c220064e7
lila-dev <75862808+lila-dev@users.noreply.github.com>
2023-01-08 19:41:35 +0100

    Add files via upload

---
+++ js/video-list.js
@@ -0,0 +1,76 @@
+let allVideos = [ 
+    {
+       name: "The mistake that ALL new developers Part 1",
+       src: "lerreur1",
+       id: "vid_1"
+    },
+    {
+       name: "The mistake that ALL new developers Part 2",
+       src: "lerreur2",
+       id: "vid_2"
+    },
+    {
+       name: "The mistake that ALL new developers Part 3",
+       src: "lerreur3",
+       id: "vid_3"
+    },
+    {
+       name: "Stop wasting time when you_re learning to code Part 1",
+       src: "bnt1",
+       id: "vid_4"
+    },
+    {
+       name: "Stop wasting time when you_re learning to code Part 2",
+       src: "bnt1",
+       id: "vid_5"
+    },
+    {
+       name: "Learn the Flexbox CSS Part 1",
+       src: "appflexbox1",
+       id: "vid_6"
+    },
+    {
+       name: "Learn the les Flexbox CSS Part 2",
+       src: "appflex2",
+       id: "vid_7"
+    },
+    {
+       name: "Learn the les Flexbox CSS Part 3",
+       src: "appflex3",
+       id: "vid_8"
+    },
+    {
+       name: "Learn CSS Position Part 1",
+       src: "learncss",
+       id: "vid_9"
+    },
+    {
+       name: "Learn CSS Position Part 2",
+       src: "learncss2",
+       id: "vid_10"
+    },
+    {
+       name: "CSS Cours 1 _ Introduction",
+       src: "CSS Cours 1 _ Introduction   First line css in Arabic Darija دورة بالدارجة لتعلم CSS(360P)",
+       id: "vid_11"
+    },
+    {
+       name: "Introduction to artificial intelligence - 1 - Decide",
+       src: "intoai",
+       id: "vid_12"
+    },
+    {
+       name: "Introduction to artificial intelligence - 2 - Decide",
+       src: "intro2ia",
+       id: "vid_13"
+    },    {
+      name: "What is artificial intelligence _ discover _1(240P)",
+      src: "cest1uoi",
+      id: "vid_14"
+   },
+   {
+      name: "What is artificial intelligence _ discover _2(240P)",
+      src: "ceatquoi2",
+      id: "vid_15"
+   }
+ ]
